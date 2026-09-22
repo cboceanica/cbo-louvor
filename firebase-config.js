5b61c33ae80435b4d07993a917deedbdaf26e15b
Thiago Nogueira <141495001+thiagonogueira87@users.noreply.github.com>
2026-09-22 10:27:01 -0300

    Update Firebase configuration and initialization
    Updated Firebase configuration with new project details and initialized Firebase app.

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,21 @@
-window.firebaseConfig = {
-  apiKey: "AIzaSy...",
-  authDomain: "escala-louvor.firebaseapp.com",
-  projectId: "escala-louvor",
-  storageBucket: "escala-louvor.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAw9KvbSY55d9x6hPDhyHh10BV2Z1k5vV8",
+  authDomain: "cbo-louvor.firebaseapp.com",
+  projectId: "cbo-louvor",
+  storageBucket: "cbo-louvor.firebasestorage.app",
+  messagingSenderId: "726867806418",
+  appId: "1:726867806418:web:c460c9df85ebc595b0c50b",
+  measurementId: "G-222J6Z2Y3M"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
